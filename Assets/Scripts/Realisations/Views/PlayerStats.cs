a5f4e816529506f6534789b05ecfdef7c810d9b5
kuzmepaul <kuzmepaul@gmail.com>
2022-09-17 22:19:18 +0300

    move player reload view, update unity, downgrade input system

--- Assets/Scripts/Realisations/Views/PlayerStats.cs
+++ Assets/Scripts/Realisations/Views/PlayerStats.cs
@@ -1,25 +1,12 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Realisations.Views
 {
     public class PlayerStats : MonoBehaviour
     {
         [SerializeField]
-        private Image gunReload;
-        [SerializeField]
         private TMP_Text healthText;
-
-        private void Awake()
-        {
-            gunReload.fillAmount = 0f;
-        }
-
-        public void UpdateGunReload(float status)
-        {
-            gunReload.fillAmount = status;
-        }
 
         public void UpdateHealth(int current, int max)
         {
